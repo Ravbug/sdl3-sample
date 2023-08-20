1f71c46ff32b26bc96ffa73e74ee2d957e2a18d4
Ravbug <ravbug@noreply.users.github.com>
2023-08-20 16:27:40 -0700

    Web fixes

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,33 @@
 #define SDL_MAIN_HANDLED
 #endif
 #include <SDL_main.h>
+#if __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 
 void SDL_Fail(){
     std::cerr << SDL_GetError() << std::endl;
     exit(1);
+}
+
+static bool app_quit = false;
+
+void main_loop() {
+    // Get events. If you are making a game, you probably want SDL_PollEvent instead of SDL_WaitEvent.
+    // you cannot use WaitEvent on Emscripten, because you cannot block the main thread there.
+
+#if __EMSCRIPTEN__
+    #define SDL_GetEvent SDL_PollEvent
+#else
+    #define SDL_GetEvent SDL_WaitEvent
+#endif
+
+    SDL_Event event;
+    while (SDL_GetEvent(&event)) {
+        if (event.type == SDL_EVENT_QUIT)
+            app_quit = true;
+        break;
+    }
 }
 
 // Note: your main function __must__ take this form, otherwise on nonstandard platforms (iOS, etc), your app will not launch.
@@ -39,20 +62,21 @@ int main(int argc, char* argv[]){
     
     std::cout << "Application started successfully!" << std::endl;
     
-    // Get events. If you are making a game, you probably want SDL_PollEvent instead of SDL_WaitEvent.
-    SDL_Event event;
-    bool quit = false;
-    while (!quit){
-        while (SDL_WaitEvent(&event)){
-            if (event.type == SDL_EVENT_QUIT)
-                quit = true;
-                break;
-        }
+#if __EMSCRIPTEN__
+    // on Emscripten, we cannot have an infinite loop in main. Instead, we must
+    // tell emscripten to call our main loop.
+    emscripten_set_main_loop(main_loop, 0, 1);
+#else
+    while (!app_quit) {
+        main_loop();
     }
+#endif
 
     // cleanup everything at the end
+#if !__EMSCRIPTEN__
     SDL_DestroyWindow(window);
     SDL_Quit();
     std::cout << "Application quit successfully!" << std::endl;
+#endif
     return 0;
 }
