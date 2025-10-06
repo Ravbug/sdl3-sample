c3dc6767de80494cb50fff5bbf6971d3887cfc7f
omkero <brigades.anony99@gmail.com>
2025-10-06 12:59:17 +0100

    implementing Dear ImGui

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,12 @@
 #include <string_view>
 #include <filesystem>
 #include <thread>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_sdl3.h"
+#include "imgui/imgui_impl_sdlrenderer3.h"
 
-constexpr uint32_t windowStartWidth = 400;
-constexpr uint32_t windowStartHeight = 400;
+constexpr uint32_t windowStartWidth = 1200;
+constexpr uint32_t windowStartHeight = 600;
 
 struct AppContext {
     SDL_Window* window;
@@ -20,6 +23,7 @@ struct AppContext {
     SDL_FRect messageDest;
     SDL_AudioDeviceID audioDevice;
     MIX_Track* track;
+    ImGuiIO& io; // imgui IO
     SDL_AppResult app_quit = SDL_APP_CONTINUE;
 };
 
@@ -120,6 +124,7 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
     MIX_PlayTrack(mixerTrack, NULL);
     
     // print some information about the window
+    float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
     SDL_ShowWindow(window);
     {
         int width, height, bbwidth, bbheight;
@@ -132,6 +137,11 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
         }
     }
 
+    IMGUI_CHECKVERSION();
+    // Setup Dear ImGui context
+    ImGui::CreateContext(); // setup ImGui context before the io
+    ImGuiIO& io = ImGui::GetIO();
+
     // set up the application data
     *appstate = new AppContext{
        .window = window,
@@ -140,18 +150,54 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
        .imageTex = tex,
        .messageDest = text_rect,
        .track = mixerTrack,
+        .io = io, // share as global reference
     };
     
-    SDL_SetRenderVSync(renderer, -1);   // enable vysnc
+    SDL_SetRenderVSync(renderer, -1);   // enable vsync
     
     SDL_Log("Application started successfully!");
+
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup scaling
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+    style.FontScaleDpi = main_scale;        // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
+    // - Read 'docs/FONTS.md' for more instructions and details. If you like the default font but want it to scale better, consider using the 'ProggyVector' from the same author!
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
+    //style.FontSizeBase = 20.0f;
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf");
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf");
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf");
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf");
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf");
+    //IM_ASSERT(font != nullptr);
 
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event* event) {
     auto* app = (AppContext*)appstate;
-    
+
+    // ImGui event handler
+    ImGui_ImplSDL3_ProcessEvent(event);
     if (event->type == SDL_EVENT_QUIT) {
         app->app_quit = SDL_APP_SUCCESS;
     }
@@ -162,19 +208,58 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event* event) {
 SDL_AppResult SDL_AppIterate(void *appstate) {
     auto* app = (AppContext*)appstate;
 
+    static bool isDemoWindow = false;
     // draw a color
     auto time = SDL_GetTicks() / 1000.f;
     auto red = (std::sin(time) + 1) / 2.0 * 255;
     auto green = (std::sin(time / 2) + 1) / 2.0 * 255;
     auto blue = (std::sin(time) * 2 + 1) / 2.0 * 255;
-    
-    SDL_SetRenderDrawColor(app->renderer, red, green, blue, SDL_ALPHA_OPAQUE);
+
+    ImVec4 clear_color = ImVec4(red, green, blue, 255.00f);
+
+    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+
+    // Start ImGui Drawing here //
+
+    if (isDemoWindow)
+        ImGui::ShowDemoWindow(&isDemoWindow);
+
+    if (ImGui::Button("Show Demo"))
+        isDemoWindow = !isDemoWindow;
+
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3("clear color", reinterpret_cast<float *>(&clear_color)); // Edit 3 floats representing a color
+
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / app->io.Framerate, app->io.Framerate);
+        ImGui::End();
+    }
+
+    // End ImGui Drawing //
+
+    SDL_SetRenderDrawColor(app->renderer, clear_color.x, clear_color.y, clear_color.z, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(app->renderer);
 
+    ImGui::Render();
     // Renderer uses the painter's algorithm to make the text appear above the image, we must render the image first.
     SDL_RenderTexture(app->renderer, app->imageTex, NULL, NULL);
     SDL_RenderTexture(app->renderer, app->messageTex, NULL, &app->messageDest);
-
+    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), app->renderer);
     SDL_RenderPresent(app->renderer);
 
     return app->app_quit;
@@ -183,6 +268,11 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 void SDL_AppQuit(void* appstate, SDL_AppResult result) {
     auto* app = (AppContext*)appstate;
     if (app) {
+        // clean ImGui
+        ImGui_ImplSDLRenderer3_Shutdown();
+        ImGui_ImplSDL3_Shutdown();
+        ImGui::DestroyContext();
+
         SDL_DestroyRenderer(app->renderer);
         SDL_DestroyWindow(app->window);
         
@@ -194,6 +284,7 @@ void SDL_AppQuit(void* appstate, SDL_AppResult result) {
 
         delete app;
     }
+
     TTF_Quit();
     MIX_Quit();
 
